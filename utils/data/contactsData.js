e84198774b4daba79d7c56f6f9ed8a363a6c47db
Pratik Doke <pratikdoke7038@gmail.com>
2024-08-16 20:27:19 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -2,7 +2,7 @@ export const contactsData = {
     email: 'pratiksoke7038@gmail.com',
     phone: '+91 7038810624',
     address: 'Pune Maharashtra',
-    github: '',
+    github: 'https://github.com/pratikdoke25',
     facebook: '',
     linkedIn: '',
     twitter: '',
